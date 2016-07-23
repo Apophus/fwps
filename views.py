3c056e9a52b6dcbbaaba0af142e7e6c4d76d2516
Apophus <bkilel12@gmail.com>
2016-07-23 22:22:23 +0300

    new file structure. Modifications in app, views and models

---
+++ views.py
@@ -0,0 +1,17 @@
+#!usr/bin/python
+
+from app import app, db
+from flask import render_template, redirect, request, url_for
+
+
+@app.route('/')
+def index():
+    return render_template('add_user.html')
+
+
+@app.route('/post_user', methods=['POST', 'GET'])
+def post_user():
+    user = User(request.form['username'], request.form['email'])
+    db.session.add(user)
+    db.session.commit()
+    return redirect(url_for('index'))
